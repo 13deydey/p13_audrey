2203c515172ec5d305dbe7de65ebe8c5d44750b6
Audrey Picarel <aud.picarel@gmail.com>
2026-02-21 11:57:53 +0100

    polices et styles css

---
+++ themes/themeperso/functions.php
@@ -0,0 +1,27 @@
+<?php
+function mon_theme_enqueue_styles() {
+    //1. ENQUEUE MAIN THEME STYLE
+    wp_enqueue_style(
+        'theme-id-style',  // Handle: identifiant unique
+        get_stylesheet_uri(), // Chemin: style.css à la racine
+        array(),
+        wp_get_theme()->get('Version')
+    );
+
+    //2. ENQUEUE FONTS 
+    wp_enqueue_style(
+        'theme-fonts',
+        get_template_directory_uri() . '/css/font.css',
+        array('theme-id-style'),
+        '1.0'
+    );
+
+    //3. ENQUEUE MAIN CSS
+    wp_enqueue_style(
+        'theme-main-css',
+        get_template_directory_uri() . '/css/main.css',
+        array('theme-fonts'),
+        '1.0'
+    );
+}
+add_action('wp_enqueue_scripts', 'mon_theme_enqueue_styles');
