1ceaf392ea958c09861ff3f12e0baeda0eace9bc
Audrey Picarel <aud.picarel@gmail.com>
2026-02-21 12:00:42 +0100

    functions.php = script js + menus

--- themes/themeperso/functions.php
+++ themes/themeperso/functions.php
@@ -25,3 +25,27 @@ function mon_theme_enqueue_styles() {
     );
 }
 add_action('wp_enqueue_scripts', 'mon_theme_enqueue_styles');
+
+function mon_theme_register_nav_menus() {
+    //Enregistre les emplacements de menus personnalisés
+        register_nav_menus(
+            array(
+                'primary' => __( 'Menu Principal', 'themeperso' ), 
+                'footer'  => __( 'Menu Pied de Page', 'themeperso' )  
+            )
+        );
+    }
+add_action( 'after_setup_theme', 'mon_theme_register_nav_menus' );
+
+//LOGO UPLOAD VIA WP CUSTOMIZER
+//ajoute le support du logo personnalisé
+function mon_theme_setup() {
+    add_theme_support( 'custom-logo', array(
+        'height'      => 75, // Hauteur maximale recommandée pour le logo
+        'width'       => 150, // Largeur maximale recommandée pour le logo
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'mon_theme_setup' );
